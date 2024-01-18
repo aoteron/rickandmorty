a203b599f50c13ccc21e28f28fcdcec332a973ee
aoteron <aoteronunez@gmail.com>
2024-01-18 14:50:54 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let mensaje = "Chanchito feliz me enseña TypeScript";
+mensaje = "chanchito triste";
+console.log(mensaje);
+mensaje = "chanchito feliz";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let mensaje: string = "Chanchito feliz me enseña TypeScript"
+
+mensaje = "chanchito triste"
+
+console.log(mensaje);
+
+mensaje = "chanchito feliz"
