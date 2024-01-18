d754648236997fb03f64232b6f1c849de8fa10bf
aoteron <aoteronunez@gmail.com>
2024-01-18 21:00:20 +0100

    reset scripts.ts

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,2 +1,10 @@
 import {Episode, Character, Location, PaginatedResponse} from "./interfaces"
 
+const urlRMAPI = 'https://rickandmortyapi.com/api/episode';
+
+const mainContainer = document.getElementById('mainContainer');
+const btnLoadEpisodes = document.getElementById('loadMoreEpisodes');
+
+////////////////////////////////////////////////
+
+
